a8661c28df7451076a1473baefcf40ec86dfbecd
Syd Amir <hemedani@gmail.com>
2020-11-21 20:14:01 +0330

    add some garbage schemas

--- funql.ts
+++ funql.ts
@@ -62,16 +62,110 @@ const scriptJsonContent = `
 }
 `;
 
+const userSchemaContent = `
+import db from "../db.ts";
+import type { City, RCity } from "./City.ts";
+import type { RState, State } from "./State.ts";
+import type { Country, RCountry } from "./Country.ts";
+import { ObjectId } from "https://deno.land/x/mongo@v0.12.1/ts/types.ts";
+
+export enum Gender {
+  Male = "Male",
+  Female = "Female",
+}
+
+export enum Level {
+  Normal = "Normal",
+  Editor = "Editor",
+  Admin = "Admin",
+  Ghost = "Ghost",
+}
+
+export interface User {
+  _id: ObjectId;
+  name: string;
+  family: string;
+  phone: number;
+  gender: Gender;
+  birthDate: Date;
+  postalCode: string;
+  address: {
+    country: Country;
+    state: State;
+    city: City;
+    text: string;
+  };
+  level: Level[];
+  email?: string;
+  password?: string;
+  isActive?: boolean;
+}
+
+export interface RUser {
+  _id: 0 | 1;
+  name?: 0 | 1;
+  family?: 0 | 1;
+  phone?: 0 | 1;
+  gender?: 0 | 1;
+  birthDate?: 0 | 1;
+  postalCode?: 0 | 1;
+  address?: {
+    country?: RCountry;
+    state?: RState;
+    city?: RCity;
+    text?: 0 | 1;
+  };
+  level?: 0 | 1;
+  email?: 0 | 1;
+  password?: 0 | 1;
+  isActive?: 0 | 1;
+}
+
+export const users = db.collection<User>("Users");
+`;
+
+const stateSchemaContent = `
+import { ObjectId } from "https://deno.land/x/mongo@v0.12.1/ts/types.ts";
+import db from "../db.ts";
+import { RCity } from "./City.ts";
+import type { Country, RCountry } from "./Country.ts";
+
+export interface State {
+  _id: ObjectId;
+  name: string;
+  enName: string;
+  country: Country;
+}
+
+export interface RState {
+  _id?: 0 | 1;
+  name?: 0 | 1;
+  enName?: 0 | 1;
+  cities?: RCity;
+  country?: RCountry;
+}
+
+export const states = db.collection<State>("States");
+`;
+
 const createProject = async (init: string | boolean) => {
   init === true ? (init = "funql") : (init = init);
   await ensureDir(`./${init}`);
+  await Deno.writeTextFile(`./${init}/index.ts`, indexContent);
+  await Deno.writeTextFile(`./${init}/scripts.json`, scriptJsonContent);
+
+  await ensureDir(`./${init}/.vscode`);
+  await Deno.writeTextFile(`./${init}/.vscode/settings.json`, vscodeSetting);
+
   await ensureDir(`./${init}/src`);
   await ensureDir(`./${init}/src/utils`);
   await ensureDir(`./${init}/utils`);
+
   await ensureDir(`./${init}/schemas`);
-  await Deno.writeTextFile(`./${init}/index.ts`, indexContent);
-  await Deno.writeTextFile(`./${init}/scripts.json`, scriptJsonContent);
-  await Deno.writeTextFile(`./${init}/.vscode/settings.json`, vscodeSetting);
+  await Deno.writeTextFile(`./${init}/schemas/user.ts`, userSchemaContent);
+  await Deno.writeTextFile(`./${init}/schemas/state.ts`, stateSchemaContent);
 };
 
-args.init ? await createProject(args.init) : console.log("nothing can do know");
+args.init
+  ? await createProject(args.init)
+  : console.log("nothing can do for know");
