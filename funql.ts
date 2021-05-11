f9644631ff21e25d0e91e5b3c7270d6539d0f45c
Syd Amir <hemedani@gmail.com>
2021-05-11 19:17:46 +0430

    🔖   add getDeclerations

--- funql.ts
+++ funql.ts
@@ -1,15 +1,17 @@
 import { parse } from "https://deno.land/std/flags/mod.ts";
 import { ensureDir } from "https://deno.land/std/fs/mod.ts";
 import { createBlog } from "./blog/index.ts";
+import { getDeclerations } from "./declerations/mod.ts";
 
 const args = parse(Deno.args);
 
 const createProject = async (init: string | boolean) => {
-  init === true ? (init = "funql") : (init = init);
+  init === true && (init = "funql");
   await ensureDir(`./${init}`);
   await createBlog(`./${init}`);
 };
 
-args.init
-  ? await createProject(args.init)
-  : console.log("nothing can do for know");
+args.init &&
+  await createProject(args.init);
+
+args.dec && await getDeclerations();
