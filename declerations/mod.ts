7c14a40426968e629640eb74d9a8d89128fbd583
Syd Amir <hemedani@gmail.com>
2021-05-11 19:13:59 +0430

    ⭐  🐞  ensureDir decleration

--- declerations/mod.ts
+++ declerations/mod.ts
@@ -1,75 +1,93 @@
 import { Project, ts } from "https://deno.land/x/ts_morph@10.0.1/mod.ts";
+import { ensureDir } from "https://deno.land/std/fs/mod.ts";
 import { constructFVSchema, convetFvObToTsOb, obToStr } from "./utils/mod.ts";
 
 //code start from here--------------------------------------------------------------------------------------------------
-export const project = new Project({
-  resolutionHost: (moduleResolutionHost, getCompilerOptions) => {
-    return {
-      resolveModuleNames: (moduleNames, containingFile) => {
-        const compilerOptions = getCompilerOptions();
-        const resolvedModules: ts.ResolvedModule[] = [];
 
-        for (const moduleName of moduleNames.map(removeTsExtension)) {
-          const result = ts.resolveModuleName(
-            moduleName,
-            containingFile,
-            compilerOptions,
-            moduleResolutionHost,
-          );
-          resolvedModules.push(result.resolvedModule!);
+export const getDeclerations = async () => {
+  console.log("                      ");
+  console.log("++++++++++++++++++++++");
+  console.log("                      ");
+  console.group("inside getDeclerations => : ");
+  console.log("                      ");
+  console.log("                      ");
+  console.groupEnd();
+  console.log("                      ");
+  console.log("----------------------");
+  console.log("                      ");
+
+  const project = new Project({
+    resolutionHost: (moduleResolutionHost, getCompilerOptions) => {
+      return {
+        resolveModuleNames: (moduleNames, containingFile) => {
+          const compilerOptions = getCompilerOptions();
+          const resolvedModules: ts.ResolvedModule[] = [];
+
+          for (const moduleName of moduleNames.map(removeTsExtension)) {
+            const result = ts.resolveModuleName(
+              moduleName,
+              containingFile,
+              compilerOptions,
+              moduleResolutionHost,
+            );
+            resolvedModules.push(result.resolvedModule!);
+          }
+
+          return resolvedModules;
+        },
+      };
+
+      function removeTsExtension(moduleName: string) {
+        if (
+          moduleName.slice(-3).toLowerCase() === ".ts" &&
+          !moduleName.startsWith("http")
+        ) {
+          return moduleName.slice(0, -3);
         }
-
-        return resolvedModules;
-      },
-    };
-
-    function removeTsExtension(moduleName: string) {
-      if (
-        moduleName.slice(-3).toLowerCase() === ".ts" &&
-        !moduleName.startsWith("http")
-      ) {
-        return moduleName.slice(0, -3);
+        return moduleName;
       }
-      return moduleName;
-    }
-  },
-});
+    },
+  });
+  // const __dirname = new URL(".", import.meta.url).pathname;
 
-const __dirname = new URL(".", import.meta.url).pathname;
+  const __dirname = ".";
 
-project.addSourceFilesAtPaths(`${__dirname}/**/*.ts`);
+  await ensureDir("declerations");
 
-const sourceFile = project.getSourceFileOrThrow(`${__dirname}/mod.ts`);
+  project.addSourceFilesAtPaths(`${__dirname}/**/*.ts`);
 
-const newSourceFile = project.createSourceFile(
-  `${__dirname}/declerations/schema.ts`,
-  undefined,
-  {
-    overwrite: true,
-  },
-);
+  const sourceFile = project.getSourceFileOrThrow(`${__dirname}/mod.ts`);
 
-const fastestValidatorObject = await constructFVSchema(
-  sourceFile,
-);
-const object = convetFvObToTsOb(
-  fastestValidatorObject,
-  {},
-);
+  const newSourceFile = project.createSourceFile(
+    `${__dirname}/declerations/schema.ts`,
+    undefined,
+    {
+      overwrite: true,
+    },
+  );
 
-//store fastest validator object
-await Deno.writeTextFile(
-  `${__dirname}/declerations/fastestValidatorSchema.json`,
-  JSON.stringify(fastestValidatorObject, null, 2),
-);
+  const fastestValidatorObject = await constructFVSchema(
+    sourceFile,
+  );
+  const object = convetFvObToTsOb(
+    fastestValidatorObject,
+    {},
+  );
 
-//store regular object
-await Deno.writeTextFile(
-  `${__dirname}/declerations/schema.json`,
-  JSON.stringify(object, null, 2),
-);
+  //store fastest validator object
+  await Deno.writeTextFile(
+    `${__dirname}/declerations/fastestValidatorSchema.json`,
+    JSON.stringify(fastestValidatorObject, null, 2),
+  );
 
-const int = newSourceFile.addInterface({ name: "Funql" });
-//await Deno.writeTextFile("schema.ts", );
-int.addProperty({ name: "schema", type: obToStr(object["schema"]) });
-await newSourceFile.save();
+  //store regular object
+  await Deno.writeTextFile(
+    `${__dirname}/declerations/schema.json`,
+    JSON.stringify(object, null, 2),
+  );
+
+  const int = newSourceFile.addInterface({ name: "Funql" });
+  //await Deno.writeTextFile("schema.ts", );
+  int.addProperty({ name: "schema", type: obToStr(object["schema"]) });
+  await newSourceFile.save();
+};
