e844f1cd53828eb38fcd038bdc2b584cb34b85d3
ehsanakefi <ehsan.akefi@gmail.com>
2021-06-04 00:02:33 +0430

    Add exec for run playgorund

--- funql.ts
+++ funql.ts
@@ -7,12 +7,14 @@ import { upgrade } from "./cli/mod.ts";
 import { log } from "./deps.ts";
 import { getSchemaDeclarations } from "./declarations/schema/mod.ts";
 import { generateDeclarations } from "./declarations/mod.ts";
+import { exec } from "https://deno.land/x/exec/mod.ts";
 
 export interface CommandArgs {
   init?: boolean | string;
   declaration?: boolean;
   upgrade?: string;
   _: (string | number)[];
+  playground?: boolean;
 }
 
 const args: CommandArgs = parse(Deno.args);
@@ -23,7 +25,22 @@ const createProject = async (init: string | boolean) => {
   await createBlog(`./${init}`);
 };
 
+const __filename = new URL(".", import.meta.url).pathname;
+
+const runPlayground = async () => {
+  console.log(__filename, "sallam", Deno.cwd());
+
+  Deno.chdir(`${Deno.cwd()}/playground`);
+
+  // Deno.run({
+  //   cmd: ["npm", "run", "start"],
+  //   // cmd: ["yarn", "start"],
+  // });
+  await exec("yarn start");
+};
+
 args.init && (await createProject(args.init));
 args.declaration && (await generateDeclarations(true, true));
+args.playground && (await runPlayground());
 
 args.upgrade && (await upgrade(args.upgrade));
