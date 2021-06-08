96679cf9468302c7aa5214ca9ce82b86936cc0c3
Syd Amir <hemedani@gmail.com>
2021-06-08 16:22:04 +0430

    add help and playground

--- funql.ts
+++ funql.ts
@@ -1,13 +1,11 @@
 import { parse } from "https://deno.land/std/flags/mod.ts";
 import { ensureDir } from "https://deno.land/std/fs/mod.ts";
 import { createBlog } from "./blog/index.ts";
-import { getRequestDeclarations } from "./declarations/request/mod.ts";
 import "./config/mod.ts";
 import { upgrade } from "./cli/mod.ts";
-import { log } from "./deps.ts";
-import { getSchemaDeclarations } from "./declarations/schema/mod.ts";
 import { generateDeclarations } from "./declarations/mod.ts";
 import { exec } from "https://deno.land/x/exec/mod.ts";
+import { runHelp } from "./help.ts";
 
 export interface CommandArgs {
   init?: boolean | string;
@@ -15,6 +13,7 @@ export interface CommandArgs {
   upgrade?: string;
   _: (string | number)[];
   playground?: boolean;
+  help?: boolean;
 }
 
 const args: CommandArgs = parse(Deno.args);
@@ -28,19 +27,21 @@ const createProject = async (init: string | boolean) => {
 const __filename = new URL(".", import.meta.url).pathname;
 
 const runPlayground = async () => {
-  console.log(__filename, "sallam", Deno.cwd());
-
-  Deno.chdir(`${Deno.cwd()}/playground`);
+  //   console.log(__filename, "sallam", Deno.cwd());
+  //
+  //   Deno.chdir(`${Deno.cwd()}/playground`);
 
   // Deno.run({
   //   cmd: ["npm", "run", "start"],
   //   // cmd: ["yarn", "start"],
   // });
-  await exec("yarn start");
+
+  await exec("deno run -A playground.ts");
 };
 
 args.init && (await createProject(args.init));
 args.declaration && (await generateDeclarations(true, true));
 args.playground && (await runPlayground());
+args.help && (runHelp());
 
 args.upgrade && (await upgrade(args.upgrade));
