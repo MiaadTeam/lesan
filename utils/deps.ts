905d1f7d73f7dcaa47d64196a9139ad6fa5bd747
hashemi <masoumeh.hashemi92@gmail.com>
2021-06-22 16:14:02 +0430

    a function for handle wmbedded documents

---
+++ utils/deps.ts
@@ -0,0 +1,3 @@
+export { Bson } from "https://deno.land/x/mongo@v0.21.2/mod.ts";
+export { Collection } from "https://deno.land/x/mongo@v0.21.2/src/collection/collection.ts";
+export * from "https://deno.land/x/mongo@v0.21.2/mod.ts";
