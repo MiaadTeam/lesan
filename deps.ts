92725acabfb38ed7e2e50f777d53b861e569db06
etefaghian <etefaghian@yahoo.com>
2021-07-18 13:38:45 +0430

    fix exports in deps.ts

--- deps.ts
+++ deps.ts
@@ -1,8 +1,5 @@
-export * from "https://deno.land/x/ts_morph@10.0.1/mod.ts";
+export * from "https://deno.land/x/ts_morph@10.0.2/mod.ts";
 export * from "https://deno.land/std/fs/mod.ts";
 export * from "https://deno.land/std@0.96.0/fmt/colors.ts";
-import * as l from "https://deno.land/std/log/mod.ts";
-export const log = l;
-
-import FastestValidator from "https://cdn.pika.dev/fastest-validator@^1.8.0";
-export const fv = new FastestValidator();
+export * as log from "https://deno.land/std/log/mod.ts";
+export { default as fv } from "https://cdn.pika.dev/fastest-validator@^1.8.0";
