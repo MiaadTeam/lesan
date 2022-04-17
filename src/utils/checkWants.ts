503bc950513dc7f533692e8a9e7115b1f00f026b
zsattari <famil.shia14@gmail.com>
2022-04-17 13:00:58 +0430

    add tsDoc to utils folder

--- src/utils/checkWants.ts
+++ src/utils/checkWants.ts
@@ -1,87 +1,115 @@
 import { throwError } from "./mod.ts";
 
+/**
+ * details of input is include set , get
+ * @public
+ */
 export interface Details {
-  set: Record<string, any>;
-  get: Record<string, any>;
+    /**
+     *  set of query
+     */
+    set: Record<string, any>;
+    /**
+     * get pf query
+     * What the client wants to return
+     */
+    get: Record<string, any>;
 }
-
+/**
+ * interface is type of input of Actions
+ * @public
+ */
 export interface Body {
-  service?: string;
-  contents: "dynamic" | "static";
-  wants: {
-    model: string;
-    act: string;
-  };
-  details: Details;
+    /**
+     * name of service
+     * "main" | "blog" | "ecommerce"
+     */
+    service?: string;
+    /**
+     * type of contents : dynamic | static
+     */
+    contents: "dynamic" | "static";
+    /**
+     * model : schema name that client wants
+     * act : name of Actions
+     */
+    wants: {
+        model: string;
+        act: string;
+    };
+    /**
+     * details of request set and get
+     */
+    details: Details;
 }
 
 const decodeBody = async (req: Request): Promise<Body> => {
-  /**
-   * @function
-   * decode body of request when content type is application/json
-   */
-  const decodeJsonBody = async () =>
-    req.body
-      ? JSON.parse(await req.text()) as Body
-      : throwError("Your request body is incorrect");
+    /**
+     * @function
+     * decode body of request when content type is application/json
+     */
+    const decodeJsonBody = async () =>
+        req.body
+            ? JSON.parse(await req.text()) as Body
+            : throwError("Your request body is incorrect");
 
-  /**
-   * @function
-   * decode all files and funql body when type of content type is multipart/form-data
-   * @remarks it puts all of files in set of details of request body
-   * @return parsed body with uploaded files
-   * @example we recommend to use postman or other client lib fot handling boundary field and other fields
-   */
-  const decodeMultiPartBody = async () => {
-    // finds boundary of form data
-    // const boundary = contentType.match(/boundary=([^\s]+)/)?.[1];
+    /**
+     * @function
+     * decode all files and funql body when type of content type is multipart/form-data
+     * @remarks it puts all of files in set of details of request body
+     * @return parsed body with uploaded files
+     * @example we recommend to use postman or other client lib fot handling boundary field and other fields
+     */
+    const decodeMultiPartBody = async () => {
+        // finds boundary of form data
+        // const boundary = contentType.match(/boundary=([^\s]+)/)?.[1];
 
-    const getFileFormData: () => Promise<Body> = async () => {
-      const fd = await req.formData();
-      // for (const f of fd.entries()) {
-      //   if (!(f[1] instanceof File)) {
-      //     continue;
-      //   }
-      //   const fileData = new Uint8Array(await f[1].arrayBuffer());
-      //   await Deno.writeFile("./files/" + f[1].name, fileData);
-      // }
-      const returnBody: (body: string) => Body = (body) => {
-        const parsedBody = JSON.parse(body) as Body;
-        parsedBody &&
-          parsedBody.details &&
-          parsedBody.details.set &&
-          (parsedBody.details.set = {
-            ...parsedBody.details.set,
-            formData: fd,
-          });
-        return parsedBody;
-      };
+        const getFileFormData: () => Promise<Body> = async () => {
+            const fd = await req.formData();
+            // for (const f of fd.entries()) {
+            //   if (!(f[1] instanceof File)) {
+            //     continue;
+            //   }
+            //   const fileData = new Uint8Array(await f[1].arrayBuffer());
+            //   await Deno.writeFile("./files/" + f[1].name, fileData);
+            // }
+            const returnBody: (body: string) => Body = (body) => {
+                const parsedBody = JSON.parse(body) as Body;
+                parsedBody
+                    && parsedBody.details
+                    && parsedBody.details.set
+                    && (parsedBody.details.set = {
+                        ...parsedBody.details.set,
+                        formData: fd,
+                    });
+                return parsedBody;
+            };
 
-      const body = fd.get("lesan-body") ? fd.get("lesan-body") as string : "{}";
+            const body = fd.get("lesan-body") ? fd.get("lesan-body") as string : "{}";
 
-      return body
-        ? returnBody(body)
-        : throwError("somthing wrong with your file");
+            return body
+                ? returnBody(body)
+                : throwError("somthing wrong with your file");
+        };
+
+        return req.body
+            ? await getFileFormData()
+            : throwError("Your body is incorrect");
     };
 
-    return req.body
-      ? await getFileFormData()
-      : throwError("Your body is incorrect");
-  };
-
-  const contentType = req.headers.get("content-type") || "";
-  return contentType.includes("application/json")
-    ? await decodeJsonBody()
-    : contentType.includes("multipart/form-data")
-    ? await decodeMultiPartBody()
-    : throwError("content type is not correct");
+    const contentType = req.headers.get("content-type") || "";
+    return contentType.includes("application/json")
+        ? await decodeJsonBody()
+        : contentType.includes("multipart/form-data")
+        ? await decodeMultiPartBody()
+        : throwError("content type is not correct");
 };
 
 export const parsBody = async (req: Request, port: number) => {
-  const parsedBody = await decodeBody(req);
-  const url = req.url.split(`${port}`)[1];
+    const parsedBody = await decodeBody(req);
+    const url = req.url.split(`${port}`)[1];
 
-  return req.method === "POST" && url === "/lesan"
-    ? parsedBody
-    : throwError("you most send a post request to /lesan url");
+    return req.method === "POST" && url === "/lesan"
+        ? parsedBody
+        : throwError("you most send a post request to /lesan url");
 };
--- src/utils/throwError.ts
+++ src/utils/throwError.ts
@@ -1,3 +1,8 @@
+/**
+ * get message and throw error
+ * @param msg - message of error
+ * @returns throwError with input message
+ */
 export const throwError = (msg?: string) => {
-  throw new Error(msg);
+    throw new Error(msg);
 };
