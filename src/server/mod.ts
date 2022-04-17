55e08e81df880c5041e295b88e808b650cc498b3
zsattari <famil.shia14@gmail.com>
2022-04-17 13:15:23 +0430

    add TsDoc to servLesan

--- src/server/mod.ts
+++ src/server/mod.ts
@@ -10,38 +10,45 @@ import { runPlayground } from "./playground/mod.ts";
 const setDb = odm().setDb;
 
 export const lesanServer = (schemasObj: ISchema, actsObj: Services) => {
-  const runServer = async (
-    { port, playground, db, typeGeneration }: {
-      port: number;
-      playground: boolean;
-      db: Database;
-      typeGeneration: boolean;
-    },
-  ) => {
-    setDb(db);
-    typeGeneration && await generateSchemTypes(schemasObj);
-    const handler = async (request: Request): Promise<Response> => {
-      try {
-        // return request.method === "GET"
-        //   ? await serveStatic(request)
-        //   : await serveLesan(request);
-        return await lesanFns(actsObj).serveLesan(request, port);
-      } catch (e) {
-        return new Response(
-          `Somthing has wrong =>> :: ${
-            e.message ||
-            "we do not know anything !!! sorry"
-          }`,
-          { status: 501 },
-        );
-      }
-    };
+    /**
+     * this function is for run Server and get request of client and send response of request for client
+     *  @param port - port of listen
+     *  @param playground - use playground or not?
+     * @param db - connection of DB
+     * @param typeGeneration -
+     */
+    const runServer = async (
+        { port, playground, db, typeGeneration }: {
+            port: number;
+            playground: boolean;
+            db: Database;
+            typeGeneration: boolean;
+        },
+    ) => {
+        setDb(db);
+        typeGeneration && await generateSchemTypes(schemasObj);
+        const handler = async (request: Request): Promise<Response> => {
+            try {
+                // return request.method === "GET"
+                //   ? await serveStatic(request)
+                //   : await serveLesan(request);
+                return await lesanFns(actsObj).serveLesan(request, port);
+            } catch (e) {
+                return new Response(
+                    `Somthing has wrong =>> :: ${
+                        e.message
+                        || "we do not know anything !!! sorry"
+                    }`,
+                    { status: 501 },
+                );
+            }
+        };
 
-    console.log(
-      `HTTP webserver running. Access it at: http://localhost:${port}/`,
-    );
-    await serve(handler, { port });
-    playground && runPlayground();
-  };
-  return runServer;
+        console.log(
+            `HTTP webserver running. Access it at: http://localhost:${port}/`,
+        );
+        await serve(handler, { port });
+        playground && runPlayground();
+    };
+    return runServer;
 };
