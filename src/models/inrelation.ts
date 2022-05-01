5fe84090dc7aef7bbb23b44727bedc3a7384728d
zsattari <famil.shia14@gmail.com>
2022-05-01 15:47:24 +0430

    edit inRelation

--- src/models/inrelation.ts
+++ src/models/inrelation.ts
@@ -9,7 +9,7 @@ import { InRelation } from "./types.ts";
  * @returns - return objects of all functions that define in this function
  */
 export const inrelationFns = (schemasObj: ISchema) => {
-  /**
+    /**
     * asign inrelation of schema that schema has relation with it example of relation of SQL that we keep foriegn key.
      * @param schemaName - Name of schema that we want add inerRealation for it
     *   @param inerRealation - record of key value of inRelation Type
@@ -25,64 +25,65 @@ export const inrelationFns = (schemasObj: ISchema) => {
     *    }
     *   }
     */
-  const addInrelations = (
-    { schemaName, inrelation }: {
-      schemaName: string;
-      inrelation: Record<string, InRelation>;
-    },
-  ) => {
-    const schemas = schemaFns(schemasObj).getSchemas();
-    const schema = schemas[schemaName];
-    if (!schema) {
-      throw new Error(`Schema ${schemaName} does not exist`);
-    }
-    schema.inrelation = inrelation;
-  };
-  /**
-   * add one innerRelation to previous inrelation of schema that schema has relation with it example of relation of SQL that we keep foriegn key.
-   * @param schemaName - Name of schema that we want add inerRealation for it
-   *   @param inerRealationName - name of inerRealtion fields
-   *   @param schemaInRelation - Name of schema that this schema has inerRealation with it
-   *   @example
-   *   example of input parameter is
-   *   {
-   *     schemaName:"city",
-   *     "inrelationName":"provice"
-   *     "schemaInRelation":"provice"
-   *     "type":"one"
-   *   }
-   */
-  const addOneInRelation = (
-    { schemaName, inrelationName, schemaInRelation, type }: {
-      schemaName: string;
-      inrelationName: string;
-      schemaInRelation: string;
-      type: "one" | "many";
-    },
-  ) => {
-    const schemas = schemaFns(schemasObj).getSchemas();
-    const schema = schemas[schemaName];
-    if (!schema) {
-      throw new Error(`Schema ${schemaName} does not exist`);
-    }
-    schema.inrelation = {
-      ...schema.inrelation,
-      [inrelationName]: { schemaName: schemaInRelation, type },
+    const addInrelations = (
+        { schemaName, inrelation }: {
+            schemaName: string;
+            inrelation: Record<string, InRelation>;
+        },
+    ) => {
+        const schemas = schemaFns(schemasObj).getSchemas();
+        const schema = schemas[schemaName];
+        if (!schema) {
+            throw new Error(`Schema ${schemaName} does not exist`);
+        }
+        schema.inrelation = inrelation;
     };
-  };
+    /**
+     * add one innerRelation to previous inrelation of schema that schema has relation with it example of relation of SQL that we keep foriegn key.
+     * @param schemaName - Name of schema that we want add inerRealation for it
+     *   @param inerRealationName - name of inerRealtion fields
+     *   @param schemaInRelation - Name of schema that this schema has inerRealation with it
+     *   @example
+     *   example of input parameter is
+     *   {
+     *     schemaName:"city",
+     *     "inrelationName":"provice"
+     *     "schemaInRelation":"provice"
+     *     "type":"one"
+     *   }
+     */
+    const addOneInRelation = (
+        { schemaName, inrelationName, schemaInRelation, type, optional }: {
+            schemaName: string;
+            inrelationName: string;
+            schemaInRelation: string;
+            type: "one" | "many";
+            optional: boolean;
+        },
+    ) => {
+        const schemas = schemaFns(schemasObj).getSchemas();
+        const schema = schemas[schemaName];
+        if (!schema) {
+            throw new Error(`Schema ${schemaName} does not exist`);
+        }
+        schema.inrelation = {
+            ...schema.inrelation,
+            [inrelationName]: { schemaName: schemaInRelation, type, optional },
+        };
+    };
 
-  /**
-   * get all of inerRealation of one schema
-   * @param schemaName - name of schema that we want inerRealation of it
-   */
-  const getInrelations = (schemaName: string) => {
-    const schemas = schemaFns(schemasObj).getSchemas();
-    schemas[schemaName].inrelation;
-  };
+    /**
+     * get all of inerRealation of one schema
+     * @param schemaName - name of schema that we want inerRealation of it
+     */
+    const getInrelations = (schemaName: string) => {
+        const schemas = schemaFns(schemasObj).getSchemas();
+        schemas[schemaName].inrelation;
+    };
 
-  return {
-    addInrelations,
-    addOneInRelation,
-    getInrelations,
-  };
+    return {
+        addInrelations,
+        addOneInRelation,
+        getInrelations,
+    };
 };
