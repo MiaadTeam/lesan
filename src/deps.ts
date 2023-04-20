f74fd02398cf6e75713f5b6aaf1bde86c31a9838
Syd Amir <hemedani@gmail.com>
2023-04-20 18:53:42 +0330

    :pushpin: increase mongodb driver version

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,3 @@
 export { ensureDir } from "https://deno.land/std@0.135.0/fs/mod.ts";
 export * from "https://deno.land/x/lestruct@v0.0.2/mod.ts";
-export * from "https://deno.land/x/mongo@v0.31.1/mod.ts";
+export * from "https://deno.land/x/mongo@v0.31.2/mod.ts";
