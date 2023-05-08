a1f5b04458333cf8249e17cfc727603a80a42fca
Syd Amir <hemedani@gmail.com>
2023-05-08 21:17:50 +0330

    :arrow_up: move to npm superstruct

--- src/deps.ts
+++ src/deps.ts
@@ -1,3 +1,4 @@
 export { ensureDir } from "https://deno.land/std@0.135.0/fs/mod.ts";
-export * from "https://deno.land/x/lestruct@v0.0.2/mod.ts";
 export * from "https://deno.land/x/mongo@v0.31.2/mod.ts";
+
+export * from "npm:superstruct@1.0.3";
