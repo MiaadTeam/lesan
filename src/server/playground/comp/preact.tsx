9a4f1b9c5cea76ebbd311b162dec1efda8afd565
MahdiTa97 <taala.mahdy@gmail.com>
2023-05-14 01:48:51 +0330

    feat: change playground styles and structure
    - change all classNames to BEM methodology
    - add new colors
    - change font
    - normalize html and body css

--- src/server/playground/comp/preact.tsx
+++ src/server/playground/comp/preact.tsx
@@ -9,8 +9,8 @@ export const Page = (
   const [selectedMethod, setSelectedMethod] = useState("");
   const [selectedSchema, setSelectedSchema] = useState("");
   const [selectedAct, setSelectedAct] = useState("");
-  const [avalibaleSetFields, setAvalibaleSetFields] = useState(null);
-  const [avalibaleGetFields, setAvalibaleGetFields] = useState(null);
+  const [availableSetFields, setAvailableSetFields] = useState(null);
+  const [availableGetFields, setAvailableGetFields] = useState(null);
   const [formData, setFormData] = useState({});
   const [headers, setHeaders] = useState<{ [key: string]: string }>({
     Authorization: "",
@@ -104,9 +104,8 @@ export const Page = (
     ]);
   };
 
-  const renderGetFileds = (getField: any, keyName: string, margin: number) => {
+  const renderGetFields = (getField: any, keyName: string, margin: number) => {
     return (
-      // style={{ marginLeft: `${margin + 10}px` }}
       <div style={{ marginLeft: `${margin + 10}px` }}>
         <h1 style={{ marginLeft: "0" }}>{keyName} :</h1>
         {Object.keys(getField["schema"]).map((childKeys) => {
@@ -126,7 +125,7 @@ export const Page = (
               />
             </div>
           ) : (
-            renderGetFileds(
+            renderGetFields(
               getField["schema"][childKeys],
               `${keyName}.${childKeys}`,
               margin + 10
@@ -137,59 +136,67 @@ export const Page = (
     );
   };
 
+  const canShowContent =
+    selectedService &&
+    selectedMethod &&
+    selectedSchema &&
+    availableSetFields &&
+    availableGetFields;
+
+  const canShowSchema = selectedService && selectedMethod;
+
+  const canShowAct = selectedService && selectedMethod && selectedSchema;
+
   return (
-    <div className="container">
-      <div className="sub-container">
-        <div className="headers">
-          <span> set headers : </span>
-          {Object.entries(headers).map(([objKey, objValue]) => {
-            return (
-              <div className="auth-input">
-                <input
-                  placeholder={objKey}
-                  id={objKey}
-                  value={objKey}
-                  name={objKey}
-                  onChange={(e: any) => {
-                    const { name, value } = e.target;
-                    objKey = value;
-                  }}
-                />
-                <input
-                  placeholder={objValue}
-                  id={objValue}
-                  value={objValue}
-                  name={objValue}
-                  onChange={(e: any) => {
-                    const { name, value } = e.target;
-                    objValue = value;
-                  }}
-                />
-                <button
-                  className="btn"
-                  onClick={() => {
-                    setHeaders((prevHeaders) => ({
-                      ...prevHeaders,
-                      [objKey]: objValue,
-                    }));
-                  }}
-                >
-                  Apply
-                </button>
-              </div>
-            );
-          })}
+    <div className="cnt">
+      <div className="sidebar">
+        <div className="sidebar__section sidebar__section--headers">
+          <div className="sidebar__section-heading">set headers</div>
+          {Object.entries(headers).map(([objKey, objValue]) => (
+            <div className="sidebar__input-double" key={objKey}>
+              <input
+                placeholder={objKey}
+                id={objKey}
+                value={objKey}
+                name={objKey}
+                onChange={(e: any) => {
+                  objKey = e.target.value;
+                }}
+              />
+              <input
+                placeholder={objValue}
+                id={objValue}
+                value={objValue}
+                name={objValue}
+                onChange={(e: any) => {
+                  objValue = e.target.value;
+                }}
+              />
+              <button
+                className="btn--add"
+                onClick={() => {
+                  setHeaders((prevHeaders) => ({
+                    ...prevHeaders,
+                    [objKey]: objValue,
+                  }));
+                }}
+              >
+                add +
+              </button>
+            </div>
+          ))}
         </div>
-        <div className="service-container">
-          <label>select service?</label>
+        <div className="sidebar__section sidebar__section--services">
+          <div className="sidebar__section-heading">select services</div>
           <select
+            className="sidebar__select"
             value={selectedService}
             onChange={(event: any) => {
               setSelectedService(event.target.value);
               setSelectedMethod("");
               setSelectedSchema("");
-              setAvalibaleGetFields(null);
-              setAvalibaleSetFields(null);
+              setAvailableGetFields(null);
+              setAvailableSetFields(null);
               setFormData({});
             }}
           >
@@ -199,16 +206,16 @@ export const Page = (
             ))}
           </select>
         </div>
-
-        <div className="service-container">
-          <label>select dynamic or static?</label>
+        <div className="sidebar__section sidebar__section--method">
+          <div className="sidebar__section-heading">select method</div>
           <select
+            className="sidebar__select"
             value={selectedMethod}
             onChange={(event: any) => {
               setSelectedMethod(event.target.value);
               setSelectedSchema("");
-              setAvalibaleGetFields(null);
-              setAvalibaleSetFields(null);
+              setAvailableGetFields(null);
+              setAvailableSetFields(null);
               setFormData({});
             }}
           >
@@ -217,121 +224,116 @@ export const Page = (
             <option value="static">static</option>
           </select>
         </div>
+        <div className="sidebar__section sidebar__section--schema">
+          <div className="sidebar__section-heading">select schema</div>
+          <select
+            className="sidebar__select"
+            disabled={!canShowSchema}
+            value={canShowSchema ? selectedSchema : undefined}
+            onChange={(event: any) => {
+              setSelectedSchema(event.target.value);
+              setAvailableGetFields(null);
+              setAvailableSetFields(null);
+              setFormData({});
+            }}
+          >
+            <option value=""></option>
+            {canShowSchema
+              ? Object.keys(
+                  (actsObj as any)[selectedService][selectedMethod]
+                ).map((schema) => <option value={schema}>{schema}</option>)
+              : null}
+          </select>
+        </div>
+        <div className="sidebar__section sidebar__section--act">
+          <div className="sidebar__section-heading">select act</div>
+          <select
+            className="sidebar__select"
+            disabled={!canShowAct}
+            value={canShowAct ? selectedAct : undefined}
+            onChange={(event: any) => {
+              const actObj = (actsObj as any)[selectedService][selectedMethod][
+                selectedSchema
+              ][event.target.value]["validator"]["schema"];
 
-        {selectedService && selectedMethod && (
-          <div className="service-container">
-            <label>select schema?</label>
-            <select
-              value={selectedSchema}
-              onChange={(event: any) => {
-                setSelectedSchema(event.target.value);
-                setAvalibaleGetFields(null);
-                setAvalibaleSetFields(null);
-                setFormData({});
-              }}
-            >
-              <option value=""></option>
-              {Object.keys(
-                (actsObj as any)[selectedService][selectedMethod]
-              ).map((schema) => (
-                <option value={schema}>{schema}</option>
-              ))}
-            </select>
-          </div>
-        )}
-
-        {selectedService && selectedMethod && selectedSchema && (
-          <div className="service-container">
-            <label>select act?</label>
-            <select
-              value={selectedAct}
-              onChange={(event: any) => {
-                const actObj = (actsObj as any)[selectedService][
-                  selectedMethod
-                ][selectedSchema][event.target.value]["validator"]["schema"];
-
-                formRef && formRef.current && formRef.current.reset();
-                setSelectedAct(event.target.value);
-                setAvalibaleGetFields(actObj["get"]["schema"]);
-                setAvalibaleSetFields(actObj["set"]["schema"]);
-                setFormData({});
-              }}
-            >
-              <option value=""></option>
-              {Object.keys(
-                (actsObj as any)[selectedService][selectedMethod][
-                  selectedSchema
-                ]
-              ).map((schema) => (
-                <option value={schema}>{schema}</option>
-              ))}
-            </select>
-          </div>
-        )}
+              formRef && formRef.current && formRef.current.reset();
+              setSelectedAct(event.target.value);
+              setAvailableGetFields(actObj["get"]["schema"]);
+              setAvailableSetFields(actObj["set"]["schema"]);
+              setFormData({});
+            }}
+          >
+            <option value=""></option>
+            {canShowAct
+              ? Object.keys(
+                  (actsObj as any)[selectedService][selectedMethod][
+                    selectedSchema
+                  ]
+                ).map((schema) => <option value={schema}>{schema}</option>)
+              : null}
+          </select>
+        </div>
       </div>
 
-      {selectedService &&
-        selectedMethod &&
-        selectedSchema &&
-        avalibaleSetFields &&
-        avalibaleGetFields && (
-          <div className="content">
-            <form ref={formRef} onSubmit={handleSubmit}>
-              <div className="">
-                <h1> set inputs :</h1>
-                <div className="get-container">
-                  {Object.keys(avalibaleSetFields).map((setField) => (
+      {/* TODO: Content and Response Have to Refactor Like Sidebar Section */}
+      {canShowContent && (
+        <div className="content">
+          <form ref={formRef} onSubmit={handleSubmit}>
+            <div className="">
+              <h1> set inputs :</h1>
+              <div className="get-container">
+                {Object.keys(availableSetFields).map((setField) => (
+                  <div className="input-container">
+                    <label htmlFor={setField}>{setField}:</label>
+                    <input
+                      placeholder={setField}
+                      id={setField}
+                      value={(formData as any)[`set.${setField}`]}
+                      name={`set.${setField}`}
+                      type={
+                        availableSetFields[setField]["type"] === "number"
+                          ? "number"
+                          : "string"
+                      }
+                      alt={availableSetFields[setField]["type"]}
+                      onChange={handleChange}
+                    />
+                  </div>
+                ))}
+              </div>
+
+              <h1> get inputs :</h1>
+              <div className="set-container">
+                {Object.keys(availableGetFields).map((getField) => {
+                  return ((availableGetFields as any)[getField] as any).type ===
+                    "enums" ? (
                     <div className="input-container">
-                      <label htmlFor={setField}>{setField}:</label>
+                      <label htmlFor={getField}>{getField}:</label>
                       <input
-                        placeholder={setField}
-                        id={setField}
-                        value={(formData as any)[`set.${setField}`]}
-                        name={`set.${setField}`}
-                        type={
-                          avalibaleSetFields[setField]["type"] === "number"
-                            ? "number"
-                            : "string"
-                        }
-                        alt={avalibaleSetFields[setField]["type"]}
+                        placeholder={getField}
+                        id={getField}
+                        value={(formData as any)[`get.${getField}`]}
+                        name={`get.${getField}`}
+                        type="number"
                         onChange={handleChange}
                       />
                     </div>
-                  ))}
-                </div>
-
-                <h1> get inputs :</h1>
-                <div className="set-container">
-                  {Object.keys(avalibaleGetFields).map((getField) => {
-                    return ((avalibaleGetFields as any)[getField] as any)
-                      .type === "enums" ? (
-                      <div className="input-container">
-                        <label htmlFor={getField}>{getField}:</label>
-                        <input
-                          placeholder={getField}
-                          id={getField}
-                          value={(formData as any)[`get.${getField}`]}
-                          name={`get.${getField}`}
-                          type="number"
-                          onChange={handleChange}
-                        />
-                      </div>
-                    ) : (
-                      renderGetFileds(
-                        (avalibaleGetFields as any)[getField],
-                        getField,
-                        0
-                      )
-                    );
-                  })}
-                </div>
-                <button className="btn btn-submit" type="submit">
-                  Submit
-                </button>
+                  ) : (
+                    renderGetFields(
+                      (availableGetFields as any)[getField],
+                      getField,
+                      0
+                    )
+                  );
+                })}
               </div>
-            </form>
-          </div>
-        )}
+              <button className="btn btn-submit" type="submit">
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      )}
       <div className="response">
         {response && (
           <div>
