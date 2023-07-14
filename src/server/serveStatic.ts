f5f238e4ccabc32853fed674c2c78fb3c7bd21ed
Syd Amir <hemedani@gmail.com>
2023-07-14 21:24:26 +0330

    :sparkles: serve static files

--- src/server/serveStatic.ts
+++ src/server/serveStatic.ts
@@ -1,11 +1,24 @@
+import { serveFile } from "https://deno.land/std@0.140.0/http/file_server.ts";
 import { Services } from "../mod.ts";
 import { ISchema } from "../models/mod.ts";
+import { throwError } from "../utils/throwError.ts";
 import {
   getClientReact,
   getCSSFile,
   getJSFile,
   runPlayground,
 } from "./playground/mod.tsx";
+
+const checkFiles = async (req: Request, staticPath: string[]) => {
+  const url = new URL(req.url);
+  const checkPath = () => {
+    const isInPath = staticPath.some(path => url.pathname.startsWith(path));
+    return isInPath;
+  };
+  return checkPath()
+    ? await serveFile(req, `${Deno.cwd()}${url.pathname}`)
+    : throwError("can not serve this path");
+};
 
 const getSchemas = (schemasObj: ISchema, actsObj: Services) => {
   return new Response(
@@ -16,6 +29,7 @@ const getSchemas = (schemasObj: ISchema, actsObj: Services) => {
   );
 };
 const checkStaticPath = async (
+  req: Request,
   url: URL,
   staticPath: string[],
   schemasObj: ISchema,
@@ -29,20 +43,18 @@ const checkStaticPath = async (
     ? await getJSFile()
     : url.pathname === `/playground/static/get/schemas`
     ? getSchemas(schemasObj, actsObj)
-    : new Response(
-      `please implement staticPath: ${staticPath} ---- for getting playground please goto: ${url.origin}/playground`,
-    );
+    : await checkFiles(req, staticPath);
 };
 
 export const serveStatic = async (
-  request: Request,
+  req: Request,
   schemasObj: ISchema,
   actsObj: Services,
   playground: boolean,
   staticPath: string[],
 ) => {
-  const url = new URL(request.url);
+  const url = new URL(req.url);
   return playground && url.pathname === "/playground"
     ? await runPlayground(url)
-    : await checkStaticPath(url, staticPath, schemasObj, actsObj);
+    : await checkStaticPath(req, url, staticPath, schemasObj, actsObj);
 };
