c816838a21910cc55a23e7b943cb1e06346d7c32
Erfangh-80 <erfangh4871gholami@gmail.com>
2023-07-23 14:57:32 +0330

    add run and add icon to project

---
+++ src/server/playground/comp/icon/RunIcon.tsx
@@ -0,0 +1,26 @@
+/** @jsx h */
+import { h } from "../../reactDeps.ts";
+
+export default function RunIcon() {
+  return (
+    <svg
+      width="25px"
+      height="25px"
+      viewBox="0 0 16 16"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+    >
+      <title>Fill 1</title>
+      <desc>Created with Sketch.</desc>
+      <g id="-" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+        <g id="Icon/playBig" fill="#8d8688">
+          <path
+            d="M6,11 L11,8 L6,5 L6,11 Z M8,14.6 C4.4,14.6 1.4,11.6 1.4,8 C1.4,4.4 4.4,1.4 8,1.4 C11.6,1.4 14.6,4.4 14.6,8 C14.6,11.6 11.6,14.6 8,14.6 L8,14.6 Z M8,0 C3.6,0 0,3.6 0,8 C0,12.4 3.6,16 8,16 C12.4,16 16,12.4 16,8 C16,3.6 12.4,0 8,0 L8,0 Z"
+            id="Fill-1"
+          ></path>
+        </g>
+      </g>
+    </svg>
+  );
+}
