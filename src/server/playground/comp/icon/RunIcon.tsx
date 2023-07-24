7b320233fd6e1db82972f462375f1ff5bba79bf8
serpico1899 <billywilder222@gmail.com>
2023-07-24 13:16:37 +0330

    🔨💄 improve and update run icon component

--- src/server/playground/comp/icon/RunIcon.tsx
+++ src/server/playground/comp/icon/RunIcon.tsx
@@ -3,24 +3,41 @@ import { h } from "../../reactDeps.ts";
 
 export default function RunIcon() {
   return (
-    <svg
-      width="25px"
-      height="25px"
-      viewBox="0 0 16 16"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-    >
-      <title>Fill 1</title>
-      <desc>Created with Sketch.</desc>
-      <g id="-" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="Icon/playBig" fill="#8d8688">
-          <path
-            d="M6,11 L11,8 L6,5 L6,11 Z M8,14.6 C4.4,14.6 1.4,11.6 1.4,8 C1.4,4.4 4.4,1.4 8,1.4 C11.6,1.4 14.6,4.4 14.6,8 C14.6,11.6 11.6,14.6 8,14.6 L8,14.6 Z M8,0 C3.6,0 0,3.6 0,8 C0,12.4 3.6,16 8,16 C12.4,16 16,12.4 16,8 C16,3.6 12.4,0 8,0 L8,0 Z"
-            id="Fill-1"
-          ></path>
-        </g>
+    <svg width="25px" viewBox="0 0 24 24" fill="none" stroke="#000000">
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <circle
+          opacity="0.5"
+          cx="12"
+          cy="12"
+          r="10"
+          stroke="#000000"
+          stroke-width="1.5"
+        ></circle>{" "}
+        <path
+          d="M13.8876 9.9348C14.9625 10.8117 15.5 11.2501 15.5 12C15.5 12.7499 14.9625 13.1883 13.8876 14.0652C13.5909 14.3073 13.2966 14.5352 13.0261 14.7251C12.7888 14.8917 12.5201 15.064 12.2419 15.2332C11.1695 15.8853 10.6333 16.2114 10.1524 15.8504C9.6715 15.4894 9.62779 14.7336 9.54038 13.2222C9.51566 12.7947 9.5 12.3757 9.5 12C9.5 11.6243 9.51566 11.2053 9.54038 10.7778C9.62779 9.26636 9.6715 8.51061 10.1524 8.1496C10.6333 7.78859 11.1695 8.11466 12.2419 8.76679C12.5201 8.93597 12.7888 9.10831 13.0261 9.27492C13.2966 9.46483 13.5909 9.69274 13.8876 9.9348Z"
+          stroke="#000000"
+          stroke-width="1.5"
+        ></path>{" "}
       </g>
     </svg>
+    // <svg width="25px" height="25px" viewBox="0 0 16 16" version="1.1">
+    //   <title>Fill 1</title>
+    //   <desc>Created with Sketch.</desc>
+    //   <g id="-" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+    //     <g id="Icon/playBig" fill="#8d8688">
+    //       <path
+    //         d="M6,11 L11,8 L6,5 L6,11 Z M8,14.6 C4.4,14.6 1.4,11.6 1.4,8 C1.4,4.4 4.4,1.4 8,1.4 C11.6,1.4 14.6,4.4 14.6,8 C14.6,11.6 11.6,14.6 8,14.6 L8,14.6 Z M8,0 C3.6,0 0,3.6 0,8 C0,12.4 3.6,16 8,16 C12.4,16 16,12.4 16,8 C16,3.6 12.4,0 8,0 L8,0 Z"
+    //         id="Fill-1"
+    //       ></path>
+    //     </g>
+    //   </g>
+    // </svg>
   );
 }
