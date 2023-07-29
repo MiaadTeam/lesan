358a18ac8714d83083132708e2539ca82b607424
serpico1899 <billywilder222@gmail.com>
2023-07-29 15:35:52 +0330

    🔨add tick icon component

---
+++ src/server/playground/comp/icon/TickIcon.tsx
@@ -0,0 +1,28 @@
+/** @jsx h */
+import { h } from "../../reactDeps.ts";
+
+export default function TickIcon() {
+  return (
+    <svg
+      width={25}
+      height={25}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M7.75 11.9999L10.58 14.8299L16.25 9.16992"
+        stroke="lightcoral"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></path>{" "}
+      <path
+        fill-rule="evenodd"
+        clip-rule="evenodd"
+        d="M23 12C23 18.0751 18.0751 23 12 23C5.92487 23 1 18.0751 1 12C1 5.92487 5.92487 1 12 1C18.0751 1 23 5.92487 23 12ZM3.00683 12C3.00683 16.9668 7.03321 20.9932 12 20.9932C16.9668 20.9932 20.9932 16.9668 20.9932 12C20.9932 7.03321 16.9668 3.00683 12 3.00683C7.03321 3.00683 3.00683 7.03321 3.00683 12Z"
+        fill="bisque"
+      />
+    </svg>
+  );
+}
