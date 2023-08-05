52f28a0c1226e9baa23c04ea2008532ff2beb6b0
serpico1899 <billywilder222@gmail.com>
2023-08-05 17:58:36 +0330

    💄improve css after add box sizing:borderbox to *

--- src/server/playground/comp/icon/SettingIcon.tsx
+++ src/server/playground/comp/icon/SettingIcon.tsx
@@ -5,6 +5,7 @@ export default function SettingIcon() {
   return (
     <svg
       width="25px"
+      height={25}
       viewBox="0 0 24 24"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
