12ff450efc788fdc115e96e8c0adc40dab1203fb
serpico1899 <billywilder222@gmail.com>
2023-08-05 14:54:41 +0330

    🔨add chevron down icon

---
+++ src/server/playground/comp/icon/ChevronDownIcon.tsx
@@ -0,0 +1,31 @@
+/** @jsx h */
+import { h } from "../../reactDeps.ts";
+
+export default function ChevronDownIcon() {
+  return (
+    <svg
+      width={25}
+      height={25}
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M6 9L12 15L18 9M12 9H12.01"
+          stroke="lightcoral"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+}
