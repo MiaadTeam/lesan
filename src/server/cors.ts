f7db5d42a27a829febe9cf96030470cbe17d848f
Syd Amir <hemedani@gmail.com>
2023-09-25 11:27:43 +0330

    :wheelchair: improve cors implemention with option to get array of white list

--- src/server/cors.ts
+++ src/server/cors.ts
@@ -1,8 +1,6 @@
-export const addCors = () => {
+export const addCors = (cors: "*" | string[], origin: string | null) => {
   const header: Headers = new Headers();
   header.set("content-type", "application/json");
-  header.set("Access-Control-Allow-Origin", "*");
-  header.set("Access-Control-Allow-Credentials", "true");
   header.set(
     "Access-Control-Allow-Headers",
     "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, token, accept, origin, Cache-Control, X-Requested-With",
@@ -11,13 +9,52 @@ export const addCors = () => {
     "Access-Control-Allow-Methods",
     "POST, OPTIONS, GET, PUT",
   );
+  if (cors === "*") {
+    header.set("Access-Control-Allow-Origin", "*");
+  } else {
+    if (origin === null) {
+      return header;
+    } else {
+      const includeOrigin = cors.includes(origin);
+      if (includeOrigin) {
+        header.set("Access-Control-Allow-Origin", origin);
+        header.set("Access-Control-Allow-Credentials", "true");
+      } else {
+        return header;
+      }
+    }
+  }
   return header;
 };
 
-export const addCorsObj = () => ({
-  "Access-Control-Allow-Origin": "*",
-  "Access-Control-Allow-Credentials": "true",
-  "Access-Control-Allow-Headers":
-    "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, token, accept, origin, Cache-Control, X-Requested-With",
-  "Access-Control-Allow-Methods": "POST, OPTIONS, GET, PUT",
-});
+export const addCorsObj = (
+  cors: "*" | string[] | undefined,
+  origin: string | null,
+) => {
+  const header: Record<string, any> = {
+    "Access-Control-Allow-Headers":
+      "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, token, accept, origin, Cache-Control, X-Requested-With",
+    "Access-Control-Allow-Methods": "POST, OPTIONS, GET, PUT",
+  };
+  if (cors === "*") {
+    header["Access-Control-Allow-Origin"] = "*";
+  } else {
+    if (origin === null) {
+      return header;
+    } else {
+      if (cors) {
+        const includeOrigin = cors.includes(origin);
+        if (includeOrigin) {
+          header["Access-Control-Allow-Origin"] = origin;
+          header["Access-Control-Allow-Credentials"] = "true";
+        } else {
+          return header;
+        }
+      } else {
+        return header;
+      }
+    }
+  }
+
+  return header;
+};
