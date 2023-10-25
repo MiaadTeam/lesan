65ddc2c7c1591d840f8e829417694c37f8fd50bd
serpico1899 <billywilder222@gmail.com>
2023-10-25 20:10:24 +0330

    add info and up icon

--- src/server/playground/comp/icon/Up2Icon.tsx
+++ src/server/playground/comp/icon/Up2Icon.tsx
@@ -23,45 +23,14 @@ export default function Up2Icon() {
         <path
           id="secondary"
           d="M10,9.66V20a1,1,0,0,0,1,1h2a1,1,0,0,0,1-1V9.66"
-          style="fill: none; stroke: #ffffff; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"
+          style="fill: none; stroke: darkred; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"
         ></path>
         <path
           id="primary"
           d="M14,9.66l3.4,2.92,2.6-3-7.35-6.3a1,1,0,0,0-1.3,0L4,9.54l2.6,3L10,9.66"
-          style="fill: none; stroke: #ffffff; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"
+          style="fill: none; stroke: darkred; stroke-linecap: round; stroke-linejoin: round; stroke-width: 2;"
         ></path>
       </g>
     </svg>
-    // <svg
-    //   fill="#000000"
-    //   viewBox="0 0 24 24"
-    //   id="up-direction-circle"
-    //   data-name="Flat Color"
-    //   xmlns="http://www.w3.org/2000/svg"
-    //   class="icon flat-color"
-    // >
-    //   <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-    //   <g
-    //     id="SVGRepo_tracerCarrier"
-    //     stroke-linecap="round"
-    //     stroke-linejoin="round"
-    //   ></g>
-    //   <g id="SVGRepo_iconCarrier">
-    //     <circle
-    //       id="primary"
-    //       cx="12"
-    //       cy="12"
-    //       r="10"
-    //       //   style="fill: #ed333b ;"
-    //       style={{ fill: "#ffffff", width: "56px" }}
-    //     ></circle>
-    //     <path
-    //       id="secondary"
-    //       d="M14,12v4a1,1,0,0,1-1,1H11a1,1,0,0,1-1-1V12H9.18a1,1,0,0,1-.76-1.65l2.82-3.27a1,1,0,0,1,1.52,0l2.82,3.27A1,1,0,0,1,14.82,12Z"
-    //       //   style="fill:#ffffff ;"
-    //       style="fill: #ed333b ;"
-    //     ></path>
-    //   </g>
-    // </svg>
   );
 }
