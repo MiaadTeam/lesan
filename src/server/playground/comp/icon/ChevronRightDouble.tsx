ea61b02b1d26dd5591c91485566c8fadddf979ef
serpico1899 <billywilder222@gmail.com>
2023-11-06 09:48:01 +0330

    🔨💄add chevron right and chevron right double icons

---
+++ src/server/playground/comp/icon/ChevronRightDouble.tsx
@@ -0,0 +1,31 @@
+/** @jsx h */
+import { h } from "../../reactDeps.ts";
+
+export default function ChevronRightDouble() {
+  return (
+    <svg
+      width="20px"
+      height="20px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M6 17L11 12L6 7M13 17L18 12L13 7"
+          stroke="#000000"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+}
---
+++ src/server/playground/comp/icon/ChevronRightIcon.tsx
@@ -0,0 +1,31 @@
+/** @jsx h */
+import { h } from "../../reactDeps.ts";
+
+export default function ChevronRightIcon() {
+  return (
+    <svg
+      width="20px"
+      height="20px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M9 6L15 12L9 18"
+          stroke="#000000"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+}
