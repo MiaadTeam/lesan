90194fc3b5ab72b67e3997e77e9b54107cebe1c0
serpico1899 <billywilder222@gmail.com>
2023-11-06 09:47:42 +0330

    🔨💄add chevron left and chevron left double icons

---
+++ src/server/playground/comp/icon/ChevronLeftIcon.tsx
@@ -0,0 +1,31 @@
+/** @jsx h */
+import { h } from "../../reactDeps.ts";
+
+export default function ChevronLeftIcon() {
+  return (
+    <svg
+      width="20px"
+      height="20px"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+      <g
+        id="SVGRepo_tracerCarrier"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      ></g>
+      <g id="SVGRepo_iconCarrier">
+        {" "}
+        <path
+          d="M15 6L9 12L15 18"
+          stroke="#000000"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+        ></path>{" "}
+      </g>
+    </svg>
+  );
+}
