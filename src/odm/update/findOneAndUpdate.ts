0057c15646b0f14116cd5ae3c612609e47673d03
Syd Amir <hemedani@gmail.com>
2023-11-23 15:05:09 +0330

    :boom: handle some situation of update

--- src/odm/update/findOneAndUpdate.ts
+++ src/odm/update/findOneAndUpdate.ts
@@ -8,6 +8,7 @@ import {
   UpdateFilter,
 } from "../../npmDeps.ts";
 import { throwError } from "../../utils/mod.ts";
+import { filterDocByProjection } from "../utils/filterDocByProjection.ts";
 
 export const findOneAndUpdate = async <PureFields extends Document = Document>(
   { db, collection, filter, update, options, projection, schemasObj }: {
@@ -34,17 +35,219 @@ export const findOneAndUpdate = async <PureFields extends Document = Document>(
     update as UpdateFilter<Document>,
     {
       ...options,
-      projection: pureDocProjection,
       returnDocument: "after",
       includeResultMetadata: true,
     },
   );
-  console.log("updatedDoc ", { updatedDoc, pureDocProjection });
   if (
     updatedDoc.lastErrorObject?.updatedExisting === false ||
     updatedDoc.lastErrorObject?.n <= 0
   ) {
     throwError("can not update this doc");
+  }
+
+  const pureUpdatedDoc = filterDocByProjection(
+    updatedDoc.value!,
+    pureDocProjection,
+  );
+
+  for (const rel in foundedSchema.relations) {
+    const actualRel = foundedSchema.relations[rel];
+    const relatedRelations = actualRel.relatedRelations;
+
+    if (actualRel.type === "single") {
+      const foundedActualRelSingleDoc = await db.collection(
+        actualRel.schemaName,
+      ).findOne({ _id: updatedDoc.value![rel]._id });
+
+      const updatePipeline = [];
+      for (const relatedRel in relatedRelations) {
+        const actualRelatedRel = relatedRelations[relatedRel];
+        if (actualRelatedRel.type === "single") {
+          if (
+            foundedActualRelSingleDoc![relatedRel]._id.equals(
+              updatedDoc.value?._id,
+            )
+          ) {
+            updatePipeline.push({ $set: { [relatedRel]: pureUpdatedDoc } });
+          }
+        } else {
+          const relatedRelDocs = foundedActualRelSingleDoc![relatedRel];
+          if (
+            relatedRelDocs && Array.isArray(relatedRelDocs)
+          ) {
+            const isRelInArrRel = relatedRelDocs.some((relatedRelDoc) =>
+              relatedRelDoc._id.equals(updatedDoc.value?._id)
+            );
+
+            if (isRelInArrRel) {
+              updatePipeline.push(
+                {
+                  $set: {
+                    [relatedRel]: {
+                      $filter: {
+                        input: `$${relatedRel}`,
+                        as: `${relatedRel}Item`,
+                        cond: {
+                          $ne: [
+                            `$$${relatedRel}Item._id`,
+                            updatedDoc.value?._id,
+                          ],
+                        },
+                      },
+                    },
+                  },
+                },
+              );
+            }
+
+            const reachedLimit =
+              relatedRelDocs.length === actualRelatedRel.limit ? true : false;
+
+            if (
+              reachedLimit && isRelInArrRel
+            ) {
+              const fieldName = actualRelatedRel.sort!.field;
+              const operator = actualRelatedRel.sort?.order === "asc"
+                ? { $gte: relatedRelDocs[relatedRelDocs.length - 1][fieldName] }
+                : {
+                  $lte: relatedRelDocs[relatedRelDocs.length - 1][fieldName],
+                };
+
+              const findOneCommand = {
+                [`${rel}._id`]: foundedActualRelSingleDoc!._id,
+                [fieldName]: operator,
+              };
+              const findNextRelatedRelForAdd = await db.collection(collection)
+                .find(findOneCommand, {
+                  projection: pureDocProjection,
+                  sort: {
+                    [fieldName]: actualRelatedRel.sort?.order === "asc"
+                      ? 1
+                      : -1,
+                  },
+                  limit: 2,
+                }).toArray();
+
+              /*
+               * 	@LOG @DEBUG @INFO
+               * 	This log written by ::==> {{ `` }}
+               *
+               * 	Please remove your log after debugging
+               */
+              console.log(" ============= ");
+              console.group(
+                "fieldName, operator, findOneCommand, findNextRelatedRelForAdd ------ ",
+              );
+              console.log();
+              console.info({
+                relatedRel,
+                fieldName,
+                operator,
+                findOneCommand,
+                findNextRelatedRelForAdd,
+                theDocumentInOperator:
+                  relatedRelDocs[relatedRelDocs.length - 1],
+                relatedRelDocs,
+              }, " ------ ");
+              console.log();
+              console.groupEnd();
+              console.log(" ============= ");
+
+              const concatArrays = findNextRelatedRelForAdd
+                ? [...findNextRelatedRelForAdd, pureUpdatedDoc]
+                : [pureUpdatedDoc];
+
+              updatePipeline.push(
+                {
+                  $set: {
+                    [relatedRel]: {
+                      $setUnion: [
+                        concatArrays,
+                        `$${relatedRel}`,
+                      ],
+                    },
+                  },
+                },
+                {
+                  $set: {
+                    [relatedRel]: {
+                      $sortArray: {
+                        input: `$${relatedRel}`,
+                        sortBy: {
+                          [actualRelatedRel.sort!.field]:
+                            actualRelatedRel.sort?.order === "asc" ? 1 : -1,
+                        },
+                      },
+                    },
+                  },
+                },
+                {
+                  $set: {
+                    [relatedRel]: {
+                      $slice: [`$${relatedRel}`, actualRelatedRel.limit],
+                    },
+                  },
+                },
+              );
+            } else {
+              updatePipeline.push(
+                {
+                  $set: {
+                    [relatedRel]: {
+                      $setUnion: [
+                        [pureUpdatedDoc],
+                        `$${relatedRel}`,
+                      ],
+                    },
+                  },
+                },
+                {
+                  $set: {
+                    [relatedRel]: {
+                      $sortArray: {
+                        input: `$${relatedRel}`,
+                        sortBy: {
+                          [actualRelatedRel.sort!.field]:
+                            actualRelatedRel.sort?.order === "asc" ? 1 : -1,
+                        },
+                      },
+                    },
+                  },
+                },
+                {
+                  $set: {
+                    [relatedRel]: {
+                      $slice: [`$${relatedRel}`, actualRelatedRel.limit],
+                    },
+                  },
+                },
+              );
+            }
+          }
+        }
+      }
+      const updatedActualRelSingleDoc = await db.collection(
+        actualRel.schemaName,
+      ).updateOne({ _id: updatedDoc.value![rel]._id }, updatePipeline);
+      /*
+       * 	@LOG @DEBUG @INFO
+       * 	This log written by ::==> {{ `` }}
+       *
+       * 	Please remove your log after debugging
+       */
+      // console.log woth no truncate
+      await Deno.stdout.write(
+        new TextEncoder().encode(
+          `the updatePipeline is : => ${
+            JSON.stringify(updatePipeline, null, 2)
+          }\n
+          and the updatedActualRelSingleDoc is => ${
+            JSON.stringify(updatedActualRelSingleDoc, null, 2)
+          }\n`,
+        ),
+      );
+    }
   }
 
   const schemaRelatedToThis = new Set<string>();
@@ -60,16 +263,11 @@ export const findOneAndUpdate = async <PureFields extends Document = Document>(
     // console.log({ schema, foundRelatedSchema });
     for (const relation in foundRelatedSchema.relations) {
       if (foundRelatedSchema.relations[relation].schemaName === collection) {
-        console.log({
-          foundRelatedSchemaMainRelationFound:
-            foundRelatedSchema.relations[relation],
-          relation,
-        });
         // TODO shayad chand bar mainRelation dade bashe ba in schema : pas bayad ye update aggregation benevisam
         if (foundRelatedSchema.relations[relation].type === "single") {
           await db.collection(schema).updateMany({
-            [`${relation}._id`]: updatedDoc.value!._id,
-          }, { $set: { [relation]: updatedDoc.value } });
+            [`${relation}._id`]: pureUpdatedDoc._id,
+          }, { $set: { [relation]: pureUpdatedDoc } });
         }
       }
     }
