dfc7888c8f51370d1d5f67b8782fa4fa9146bd8f
Syd Amir <hemedani@gmail.com>
2023-12-04 19:32:56 +0330

    :speech_balloon: add some documantation examples

---
+++ examples/document/03-add-first-relation.ts
@@ -0,0 +1,120 @@
+import {
+  ActFn,
+  lesan,
+  MongoClient,
+  number,
+  object,
+  ObjectId,
+  objectIdValidation,
+  RelationDataType,
+  RelationSortOrderType,
+  string,
+} from "../../mod.ts";
+
+const coreApp = lesan();
+
+const client = await new MongoClient("mongodb://127.0.0.1:27017/").connect();
+
+const db = client.db("documentExamples");
+
+coreApp.odm.setDb(db);
+
+const countryCityPure = {
+  name: string(),
+  population: number(),
+  abb: string(),
+};
+
+const countryRelations = {};
+
+const countries = coreApp.odm.newModel(
+  "country",
+  countryCityPure,
+  countryRelations,
+);
+
+const cityRelations = {
+  country: {
+    optional: false,
+    schemaName: "country",
+    type: "single" as RelationDataType,
+    relatedRelations: {
+      cities: {
+        type: "multiple" as RelationDataType,
+        limit: 50,
+        sort: {
+          field: "_id",
+          order: "desc" as RelationSortOrderType,
+        },
+      },
+    },
+  },
+};
+
+const cities = coreApp.odm.newModel(
+  "city",
+  countryCityPure,
+  cityRelations,
+);
+
+const addCountryValidator = () => {
+  return object({
+    set: object(countryCityPure),
+    get: coreApp.schemas.selectStruct("country", 1),
+  });
+};
+
+const addCountry: ActFn = async (body) => {
+  const { name, population, abb } = body.details.set;
+  return await countries.insertOne({
+    doc: {
+      name,
+      population,
+      abb,
+    },
+    projection: body.details.get,
+  });
+};
+
+coreApp.acts.setAct({
+  schema: "country",
+  actName: "addCountry",
+  validator: addCountryValidator(),
+  fn: addCountry,
+});
+
+const addCityValidator = () => {
+  return object({
+    set: object({
+      ...countryCityPure,
+      country: objectIdValidation,
+    }),
+    get: coreApp.schemas.selectStruct("city", 1),
+  });
+};
+
+const addCity: ActFn = async (body) => {
+  const { country, name, population, abb } = body.details.set;
+
+  return await cities.insertOne({
+    doc: { name, population, abb },
+    projection: body.details.get,
+    relations: {
+      country: {
+        _ids: new ObjectId(country),
+        relatedRelations: {
+          cities: true,
+        },
+      },
+    },
+  });
+};
+
+coreApp.acts.setAct({
+  schema: "city",
+  actName: "addCity",
+  validator: addCityValidator(),
+  fn: addCity,
+});
+
+coreApp.runServer({ port: 1366, typeGeneration: true, playground: true });
