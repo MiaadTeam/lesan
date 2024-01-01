ee3b585a0c8a59982fcf7bd2cb1b9543fea36c2c
rezahatampoor <rezahtms@gmail.com>
2024-01-01 19:43:52 +0330

    Implementation test for pure models

---
+++ src/models/pure/__test__/getPureModel.test.ts
@@ -0,0 +1,27 @@
+import {
+  assertInstanceOf,
+  assertThrows,
+} from "https://deno.land/std@0.130.0/testing/asserts.ts";
+import { schemaMockData } from "../../mainRelations/__test__/getMainRelations.test.ts";
+import { getPureModel } from "../getPureModel.ts";
+
+Deno.test({
+  name: "getPureModel should return Pure of user from schemaMockData",
+  fn() {
+    const getSchemaPureModel = getPureModel(schemaMockData, "user");
+    // console.log({ getSchemaPureModel });
+    assertInstanceOf(getSchemaPureModel, Object);
+  },
+});
+
+Deno.test({
+  name: "getMainRelations should throw err when we want access to notUser",
+  fn() {
+    const getNotSchemaPureModel = () => getPureModel(schemaMockData, "notUser");
+    assertThrows(
+      getNotSchemaPureModel,
+      Error,
+      "Schema notUser is not exist in the Schema Object"
+    );
+  },
+});
