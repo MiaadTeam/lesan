3f8e37ba8ec0ee50227794e44944e73a1184793e
rezahatampoor <rezahtms@gmail.com>
2024-01-06 14:42:14 +0330

    write deno Test for getNumericPosition

---
+++ src/utils/__test__/getNumericPosition.test.ts
@@ -0,0 +1,16 @@
+import { assertEquals } from "https://deno.land/std@0.130.0/testing/asserts.ts";
+import { getNumericPosition } from "../getNumericPosition.ts";
+
+Deno.test("getNumericPosition - ascending order", () => {
+  const arr = [{ value: 5 }, { value: 15 }, { value: 25 }, { value: 35 }];
+  assertEquals(getNumericPosition(arr, 25, "value", "asc"), 2);
+  assertEquals(getNumericPosition(arr, 35, "value", "asc"), 3);
+  assertEquals(getNumericPosition(arr, 5, "value", "asc"), 0);
+});
+
+Deno.test("getNumericPosition - descending order", () => {
+  const arr = [{ value: 35 }, { value: 25 }, { value: 15 }, { value: 5 }];
+  assertEquals(getNumericPosition(arr, 25, "value", "desc"), 1);
+  assertEquals(getNumericPosition(arr, 35, "value", "desc"), 0);
+  assertEquals(getNumericPosition(arr, 5, "value", "desc"), 3);
+});
