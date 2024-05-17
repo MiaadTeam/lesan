096dad45bfe36d0bea4a37c1fe94baad6d6d7cb0
Syd Amir <hemedani@gmail.com>
2024-05-17 18:43:09 +0330

    :sparkles: add createIndex to new model

--- src/odm/mod.ts
+++ src/odm/mod.ts
@@ -1,48 +1,44 @@
-import { Database } from "../deps.ts";
-import {
-	IPureFields,
-	IRelationsFileds,
-	TRelation,
-	TSchemas,
-} from "../models/mod.ts";
+import { Db } from "../npmDeps.ts";
+import { IPureFields, IRelationsFileds, TSchemas } from "../models/mod.ts";
 import { schemaFns } from "../models/mod.ts";
 import { assert, enums } from "../npmDeps.ts";
 import { throwError } from "../utils/throwError.ts";
-import { newModel } from "./newModel/mod.ts";
+import { newModel, OptionType } from "./newModel/mod.ts";
 
 export const odm = (schemasObj: TSchemas) => {
-	let mongoDb: Database;
+  let mongoDb: Db;
 
-	const setDb = (db: Database) => (mongoDb = db);
+  const setDb = (db: Db) => (mongoDb = db);
 
-	const getDbClient = () => mongoDb;
+  const getDbClient = () => mongoDb;
 
-	const getCollection = (collection: string) => {
-		const db = getDbClient();
-		const getSchemas = enums(schemaFns(schemasObj).getSchemasKeys());
-		assert(collection, getSchemas);
-		return db
-			? db.collection(collection)
-			: throwError("No database connection");
-	};
+  const getCollection = (collection: string) => {
+    const db = getDbClient();
+    const getSchemas = enums(schemaFns(schemasObj).getSchemasKeys());
+    assert(collection, getSchemas);
+    return db
+      ? db.collection(collection)
+      : throwError("No database connection");
+  };
 
-	return {
-		setDb,
-		getCollection,
-		newModel: <PF extends IPureFields, TR extends IRelationsFileds>(
-			name: string,
-			pureFields: PF,
-			relations: TR,
-			options?: { excludes?: Partial<(keyof PF)>[] },
-		) => newModel<PF, TR>(
-			mongoDb,
-			schemasObj,
-			name,
-			pureFields,
-			relations,
-			options,
-		),
-	};
+  return {
+    setDb,
+    getCollection,
+    newModel: <PF extends IPureFields, TR extends IRelationsFileds>(
+      name: string,
+      pureFields: PF,
+      relations: TR,
+      options?: OptionType<PF>,
+    ) =>
+      newModel<PF, TR>(
+        mongoDb,
+        schemasObj,
+        name,
+        pureFields,
+        relations,
+        options,
+      ),
+  };
 };
 
 export * from "./utils/mod.ts";
