26a0f6608a1b1a1d7c330eca31d4e48606ed54c3
Syd Amir <hemedani@gmail.com>
2024-06-09 11:45:44 +0330

    :chart_with_upwards_trend: export deep partial

--- src/types/mod.ts
+++ src/types/mod.ts
@@ -74,7 +74,7 @@ export const generateSchemTypes = async (
   `;
 
   str = str + `
-type DeepPartial<T> = {
+export type DeepPartial<T> = {
 	[P in keyof T]?: T[P] extends object ? DeepPartial<T[P]> : T[P];
 };
 
